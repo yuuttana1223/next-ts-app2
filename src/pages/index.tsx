8032f2ad7608462247407a226df531f8496250a4
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-11-29 00:14:10 +0900

    ✨ APIをたたいた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,45 +2,47 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import classes from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
-import { Footer } from "src/components/Footer";
 import { useCounter } from "src/hooks/useCounter";
 import { useInputArray } from "src/hooks/useInputArray";
+import { useState, useCallback, useEffect } from "react";
 
 type Props = ReturnType<typeof useCounter> & ReturnType<typeof useInputArray>;
 
+type Post = {
+  useId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
 const Home: NextPage<Props> = (props) => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const fetchPosts = useCallback(async (): Promise<void> => {
+    const res: Response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const json: Post[] = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    fetchPosts();
+  }, [fetchPosts]);
+
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-
-      {props.isShow && <h1>{props.count}</h1>}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      <Main
-        page="index"
-        number={1111}
-        array={[1, 2, 3]}
-        obj={{ foo: "foo", bar: "bar" }}
-        // boolean={true} // trueがデフォルト
-        boolean
-        // code={<code className={classes.code}>pages/index.tsx</code>}
-        onClick={() => alert("クリック")}
-      />
-      <Footer />
+      {posts.length > 0 && (
+        <ol>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ol>
+      )}
     </div>
   );
 };
