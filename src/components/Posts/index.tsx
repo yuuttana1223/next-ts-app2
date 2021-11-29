0b608ca9dc444dc7525803b1df12198e52845c95
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-11-29 18:35:44 +0900

    ♻️ useReducerで記述

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, VFC } from "react";
+import { useCallback, useEffect, VFC, useReducer } from "react";
 
 type Post = {
   useId: number;
@@ -7,47 +7,89 @@ type Post = {
   body: string;
 };
 
+type State = {
+  data: Post[];
+  loading: boolean;
+  error: unknown;
+};
+
+type Action = State & {
+  type: "end" | "error";
+};
+
+const initialState: State = {
+  data: [],
+  loading: true,
+  error: null,
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "end":
+      return {
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+    case "error":
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    default:
+      throw new Error("No such action type!");
+  }
+};
+
 export const Posts: VFC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<unknown>();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
 
   const fetchPosts = useCallback(async (): Promise<void> => {
     try {
       const res: Response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
+        "https://jsonplaceholder.typicode.com/postsa"
       );
       if (!res.ok) {
         throw new Error("エラーが発生したため、データの取得に失敗しました");
       }
       const json: Post[] = await res.json();
-      setPosts(json);
+      dispatch({
+        ...state,
+        type: "end",
+        data: json,
+      });
     } catch (error: unknown) {
-      setError(error);
-    } finally {
-      setLoading(false);
+      dispatch({
+        ...state,
+        type: "end",
+        error,
+      });
     }
-  }, []);
+  }, [state]);
 
   useEffect(() => {
     fetchPosts();
   }, [fetchPosts]);
 
-  if (loading) {
+  console.log("ff");
+
+  if (state.loading) {
     return <div>ローディング中</div>;
   }
 
-  if (error instanceof Error) {
-    return <div>{error.message}</div>;
+  if (state.error instanceof Error) {
+    return <div>{state.error.message}</div>;
   }
 
-  if (posts.length === 0) {
+  if (state.data.length === 0) {
     return <div>データは空です</div>;
   }
 
   return (
     <ol>
-      {posts.map((post) => (
+      {state.data.map((post) => (
         <li key={post.id}>{post.title}</li>
       ))}
     </ol>
