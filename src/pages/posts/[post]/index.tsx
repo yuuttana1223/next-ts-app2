aae392950bb0aecbed61991e852efbd5f98d15cb
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-01 14:20:21 +0900

    🚚 投稿系ページの名前変更

--- src/pages/posts/[post]/index.tsx
+++ src/pages/posts/[post]/index.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
 import classes from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
-import { Post } from "src/components/Post";
+import { Post as PostComponent } from "src/components/Post";
 
-const PostPage: NextPage = () => {
+const Post: NextPage = () => {
   return (
     <div className={classes.container}>
       <Header />
-      <Post />
+      <PostComponent />
     </div>
   );
 };
 
-export default PostPage;
+export default Post;
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,18 +2,18 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import classes from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
-import { Posts } from "src/components/Posts";
+import { Posts as PostsComponent } from "src/components/Posts";
 
-const PostsPage: NextPage = () => {
+const Posts: NextPage = () => {
   return (
     <div className={classes.container}>
       <Head>
         <title>Posts Page</title>
       </Head>
       <Header />
-      <Posts />
+      <PostsComponent />
     </div>
   );
 };
 
-export default PostsPage;
+export default Posts;
