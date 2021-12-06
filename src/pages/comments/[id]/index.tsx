d7ee2a5fa70b827424cfc9cbb74fb3152d4241ea
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-06 21:20:44 +0900

    ⚡️ コメントの個別ページをSG化

--- src/pages/comments/[id]/index.tsx
+++ src/pages/comments/[id]/index.tsx
@@ -1,12 +1,48 @@
-import type { NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { Header } from "src/components/Header";
+import { API_URL } from "src/constants";
+import { SWRConfig } from "swr";
+import { Comment } from "src/types/comment";
 import { Comment as CommentComponent } from "src/components/Comment";
 
-const Comment: NextPage = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res: Response = await fetch(`${API_URL}/comments`);
+  const json: Comment[] = await res.json();
+  const paths = json.map((comment) => {
+    return { params: { id: comment.id.toString() } };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const COMMENT_API_URL: string = `${API_URL}/comments/${context.params?.id}`;
+  const res: Response = await fetch(COMMENT_API_URL);
+  const comment: Comment = await res.json();
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: comment,
+      },
+    },
+  };
+};
+
+type Props = {
+  fallback: {
+    [x: string]: Comment;
+  };
+};
+
+const Comment: NextPage<Props> = ({ fallback }) => {
   return (
     <div>
-      <Header />
-      <CommentComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <CommentComponent />
+      </SWRConfig>
     </div>
   );
 };
