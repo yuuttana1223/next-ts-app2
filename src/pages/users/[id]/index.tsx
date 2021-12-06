97f8ff763388411b09978babe264b09c611e82cb
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-06 15:58:40 +0900

    ✨ ユーザー詳細ページでSSRを実践

--- src/pages/users/[id]/index.tsx
+++ src/pages/users/[id]/index.tsx
@@ -1,13 +1,36 @@
-import type { NextPage } from "next";
+import type { NextPage, GetServerSideProps } from "next";
 import { Header } from "src/components/Header";
 import { User as UserComponent } from "src/components/User";
+import { API_URL } from "src/constants";
+import { User } from "src/types/user";
+import { SWRConfig } from "swr";
 
-const User: NextPage = () => {
+// サーバ側で動いている
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const API_USER_URL: string = `${API_URL}/users/${context.query.id}`;
+  const res: Response = await fetch(API_USER_URL);
+  const user: User = await res.json();
+  return {
+    props: {
+      fallback: {
+        [API_USER_URL]: user,
+      },
+    },
+  };
+};
+
+type Props = {
+  fallback: {
+    [x: string]: User;
+  };
+};
+
+const User: NextPage<Props> = ({ fallback }) => {
   return (
-    <div>
+    <SWRConfig value={{ fallback }}>
       <Header />
       <UserComponent />
-    </div>
+    </SWRConfig>
   );
 };
 
