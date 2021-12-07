4a4e51a052bdea9b46628d50b55f061b4baaab40
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-07 18:34:44 +0900

    ♻️ Update fetcher.

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,11 +1,8 @@
-import { Post } from "src/types/post";
-import { User } from "src/types/user";
-
 export const fetcher = async (url: string): Promise<any> => {
   const res: Response = await fetch(url);
   if (!res.ok) {
     throw new Error("エラーが発生したため、データの取得に失敗しました。");
   }
-  const json: Post | User | Post[] = await res.json();
+  const json = await res.json();
   return json;
 };
